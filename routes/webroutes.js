58ff980cd126f194c03f01b4f890f197f6a22053
Jahmai OSullivan <jaosulli@microsoft.com>
2015-12-10 17:58:54 -0500

    convert routes to typescript

--- routes/webroutes.js
+++ routes/webroutes.js
@@ -1,55 +1,58 @@
 var viewsFolder = 'auth';
-var passport = require('passport');
 var userservice = new (require('../services/userservice'))();
-module.exports = function (router) {
+module.exports = router => {
     // define the home page route
-    router.get('', IsAnonymous, (req, res) => {
+    router.get('', isAnonymous, (req, res) => {
         res.render('home/landingpg', res);
     });
     /*************** Auth *****************************/
-    router.get('/login', IsAnonymous, (req, res) => {
+    router.get('/login', isAnonymous, (req, res) => {
         res.render(viewsFolder + '/login', {});
     });
-    router.get('/register', IsAnonymous, (req, res) => {
+    router.get('/register', isAnonymous, (req, res) => {
         res.render(viewsFolder + '/register', {});
     });
-    router.post('/login', IsAnonymous, passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/login',
-        failureFlash: true
-    }));
+    router.post('/login', isAnonymous, (req, res) => {
+        req.session.passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/login',
+            failureFlash: true
+        });
+    });
     //logs user out of site, deleting them from the session, and returns to homepage
-    router.get('/logout', IsAuthenticated, (req, res) => {
+    router.get('/logout', isAuthenticated, (req, res) => {
         console.log("LOGGING OUT " + req.user.email);
         req.logout();
         res.redirect('/');
         req.session.notice = "You have successfully been logged out!";
     });
-    router.post('/sign-up', IsAnonymous, passport.authenticate('local-signup', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    }));
+    router.post('/sign-up', isAnonymous, (req, res) => {
+        req.session.passport.authenticate('local-signup', {
+            successRedirect: '/',
+            failureRedirect: '/login'
+        });
+    });
     /************************** Users *************************************************/
-    router.get('/users', IsAuthenticated, (req, res) => {
+    router.get('/users', isAuthenticated, (req, res) => {
         userservice.All()
-            .then(function (users) {
+            .then(users => {
             res.render('users/list', {
                 users: users
             });
         });
     });
-    router.get('/users/profile', IsAuthenticated, (req, res) => {
+    router.get('/users/profile', isAuthenticated, (req, res) => {
         res.render('users/profile', { user: req.session.user });
     });
     return router;
 };
-function IsAuthenticated(req, res, next) {
-    if (req.user)
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated())
         return next();
     res.redirect('/login');
 }
-function IsAnonymous(req, res, next) {
-    if (!req.user)
+function isAnonymous(req, res, next) {
+    if (req.isUnauthenticated())
         return next();
     res.redirect('/users/profile');
 }
--- routes/webroutes.ts
+++ routes/webroutes.ts
@@ -1,56 +1,59 @@
 ﻿import express = require('express');
 var viewsFolder = 'auth';
-var passport = require('passport');
 var userservice = new (require('../services/userservice'))();
 
-module.exports = function (router) {
+module.exports = router => {
 
     // define the home page route
-    router.get('', IsAnonymous, (req: express.Request, res: express.Response) => {
+    router.get('', isAnonymous, (req: express.Request, res: express.Response) => {
         res.render('home/landingpg', res);
     });
     
     
     /*************** Auth *****************************/
-    router.get('/login', IsAnonymous,(req: express.Request, res: express.Response) => {
+    router.get('/login', isAnonymous,(req: express.Request, res: express.Response) => {
         res.render(viewsFolder + '/login', {});
     });
     
-    router.get('/register', IsAnonymous, (req: express.Request, res: express.Response) => {
+    router.get('/register', isAnonymous, (req: express.Request, res: express.Response) => {
         res.render(viewsFolder + '/register', {});
     });
     
-    router.post('/login', IsAnonymous, passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/login',
-        failureFlash: true
-    }));
+    router.post('/login', isAnonymous, (req: express.Request, res: express.Response) => {
+        req.session.passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/login',
+            failureFlash: true
+        })
+    });
     
     //logs user out of site, deleting them from the session, and returns to homepage
-    router.get('/logout', IsAuthenticated, (req: express.Request, res: express.Response) => {
+    router.get('/logout', isAuthenticated, (req: express.Request, res: express.Response) => {
         console.log("LOGGING OUT " + req.user.email);
         req.logout();
         res.redirect('/');
         req.session.notice = "You have successfully been logged out!";
     });
     
-    router.post('/sign-up', IsAnonymous, passport.authenticate('local-signup', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    }));
-    
-    
-    /************************** Users *************************************************/
-    router.get('/users', IsAuthenticated, (req: express.Request, res: express.Response) => {
-        userservice.All()
-            .then(function (users) {
-            res.render('users/list', {
-                users: users
-            });
+    router.post('/sign-up', isAnonymous, (req: express.Request, res: express.Response) => {
+        req.session.passport.authenticate('local-signup', {
+            successRedirect: '/',
+            failureRedirect: '/login'
         });
     });
     
-    router.get('/users/profile', IsAuthenticated, (req: express.Request, res: express.Response) => {
+    
+    /************************** Users *************************************************/
+    router.get('/users', isAuthenticated, (req: express.Request, res: express.Response) => {
+        userservice.All()
+            .then(users => {
+                res.render('users/list', {
+                    users: users
+                });
+            });
+    });
+    
+    router.get('/users/profile', isAuthenticated, (req: express.Request, res: express.Response) => {
         res.render('users/profile', { user: req.session.user });
     });
 
@@ -58,14 +61,14 @@ module.exports = function (router) {
 }
 
 
-function IsAuthenticated(req: express.Request, res: express.Response, next) {
-    if (req.user)
+function isAuthenticated(req: express.Request, res: express.Response, next) {
+    if (req.isAuthenticated())
         return next();
     
     res.redirect('/login');
 }
-function IsAnonymous(req: express.Request, res: express.Response, next) {
-    if (!req.user)
+function isAnonymous(req: express.Request, res: express.Response, next) {
+    if (req.isUnauthenticated())
         return next();
     
     res.redirect('/users/profile');
